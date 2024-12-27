4ca3770588a1ae8951277a2c15a682e1296a1487
Nick Hibbits <172406954+nicholashibbits1@users.noreply.github.com>
2024-12-26 19:27:21 -0800

    add base styles

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,9 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}></main>{" "}
+      <main className={styles.main}>Clarity Dose Radio</main>
     </div>
   );
 }
